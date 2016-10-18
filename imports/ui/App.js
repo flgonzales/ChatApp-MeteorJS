eddbeb20e7e491c665d75362fff83f6873e3523a
Fabiano Gonzalez <fabianolgonzales@gmail.com>
2016-10-18 14:39:23 +0200

    create App.js with ChatBox render

---
+++ imports/ui/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Chatbox from './Chatbox.js';
+
+// App component - represents the whole App
+export default class App extends Component {
+  getChatbox() {
+    return [
+      { _id: 1, text: 'Insert here your message'}
+    ];
+  }
+
+  renderChatbox() {
+    return this.getChatbox().map((chatbox) => (
+      <Chatbox key={chatbox._id} chatbox={chatbox} />
+    ));
+  }
+
+  render () {
+    return (
+      <div className="container">
+        <header>
+          <h1>ChatApp</h1>
+        </header>
+
+        <p>
+          {this.renderChatbox ()}
+        </p>
+    ),
+  }
+}
