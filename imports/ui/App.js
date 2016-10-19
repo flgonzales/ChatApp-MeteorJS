fa6401430e5117fb7026ba3d95a84a55c00e3cf6
Fabiano Gonzalez <fabianolgonzales@gmail.com>
2016-10-20 00:23:46 +0200

    defining views

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Chatbox from './Chatbox.js';
-import MessageInput from '.MessageInput.js';
+import Chatbox from './Chatbox.jsx';
+import MessageInput from './MessageInput.jsx';
 
 // App component - represents the whole App
 export default class App extends Component {
