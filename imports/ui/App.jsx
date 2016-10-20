ae1f0152f92e64cf30eff9bbe42f3083960ae111
Fabiano Gonzalez <fabianolgonzales@gmail.com>
2016-10-20 04:04:34 +0200

    App.jsx

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,32 +1,62 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+import {createContainer} from 'meteor/react-meteor-data';
+
+import {Messages} from '../api/messages.js';
 import Chatbox from './Chatbox.jsx';
-// import MessageInput from '.MessageInput.jsx'
+import MessageInput from './MessageInput.jsx';
 
 // App component - represents the whole App
-export default class App extends Component {
-  getChatbox() {
-    return (
-      { _id: 1, text: 'Insert here your message'}
-    );
+class App extends Component {
+  handleSubmit(event) {
+    event.preventDefault ();
+
+    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+    Messages.insert({
+      text,
+      createdAt: new Date(),
+    });
+
+    ReactDOM.findDOMNode(this.refs.textInput).value = '';
   }
 
-//  renderChatbox() {
-//    return this.getChatbox().map((chatbox) => (
-//      <Chatbox key={chatbox._id} chatbox={chatbox} />
-//    ));
-//  }
-
-  render () {
-    return (
-      <div className="container">
-        <header>
-          <h1>ChatApp</h1>
-        </header>
-
-        <p>
-          {this.renderChatbox ()}
-        </p>
-      </div>
-    );
+  renderMessages(){
+    return this.props.messages.map((message) => (
+      <Message key={message._id} message={message} />
+    ));
   }
+
+render() {
+  return (
+    <div className="container">
+      <header>
+        <h1>ChatApp</h1>
+      </header>
+
+      <ul>
+        {this.renderMessages()}
+      </ul>
+      <footer>
+        <form className="new-message" onSubmit={this.handleSubmit.bind(this)}>
+          <input
+            type="text"
+            ref="textInput"
+            placeholder="Type your message here"
+          />
+        </form>
+      </footer>
+    </div>
+  );
 }
+}
+
+App.PropTypes = {
+  tasks: PropTypes.array.isRequired,
+};
+
+export default createContainer(() => {
+  return {
+    messages: Messages.find().fetch(),
+  };
+}, App);
